b5d6babfb46fbcd93f683df2da6d56704b3612e8
frknygt <avfurkanyigit@gmail.com>
2020-07-05 12:02:19 +0100

    second update after mr bahadir

--- src/test/java/com/bitrix24/stepdefinitions/ServiceStepDef.java
+++ src/test/java/com/bitrix24/stepdefinitions/ServiceStepDef.java
@@ -42,7 +42,7 @@ public class ServiceStepDef {
     @Then("ticket should be seen on my-Orders Page")
     public void ticketShouldBeSeenOnMyOrdersPage() {
 
-        Assert.assertTrue(servicePage.ticketList.get(0).contains(servicePage.createTicket()));
+        Assert.assertTrue(servicePage.ticketList.get(0).getText().contains(servicePage.createTicket() ));
 
 
 
